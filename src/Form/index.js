ed10099f91353c7a04bfb1de901d71917abb4f83
Adam Musial <adammusial@wp.pl>
2023-01-26 21:35:13 +0100

    autofocus to input added

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Form = () => (
     <form className="form">
-        <input className="form__input" placeholder="Co jest do zrobienia?" />
+        <input className="form__input" placeholder="Co jest do zrobienia?" autoFocus />
         <button className="form__addButton">Dodaj zadanie</button>
     </form>
 );
