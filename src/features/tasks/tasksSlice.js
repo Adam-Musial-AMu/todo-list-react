c8afe3367e230231b5387bb206c1b43e19e5c4a4
Adam Musial <adammusial@wp.pl>
2023-08-15 22:57:04 +0200

    creation of reducers

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -4,14 +4,35 @@ const tasksSlice = createSlice({
     name: 'tasks',
     initialState: {
         tasks: [],
+        hideDoneTasks: false,
     },
     reducers: {
         addTask: ({ tasks }, { payload }) => {
             tasks.push(payload);
         },
+
+        toggleHideDoneTasks: state => {
+            state.hideDoneTasks = !state.hideDoneTasks;
+        },
+
+        toggleTaskDone: (state, action) => {
+            const index = state.tasks.findIndex(task => task.id === action.payload);
+            state.tasks[index].done = !state.tasks[index].done;
+        },
+
+        removeTask: (state, action) => {
+            const index = state.tasks.findIndex(task => task.id === action.payload);
+            state.tasks.splice(index, 1);
+        },
+
+        markAllTasksDone: (state) => {
+            state.tasks.forEach(task => {
+                task.done = true;
+            });
+        },
     },
 });
 
-export const { addTask } = tasksSlice.actions;
+export const { addTask, toggleHideDoneTasks, toggleTaskDone, removeTask, markAllTasksDone } = tasksSlice.actions;
 export const selectTasks = state => state.tasks;
 export default tasksSlice.reducer;
