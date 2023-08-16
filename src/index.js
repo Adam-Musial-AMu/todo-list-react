a57813bfd3b2042abcc61e70b2e347a9cf9e8273
Adam Musial <adammusial@wp.pl>
2023-08-16 20:05:15 +0200

    using styled-normalize instead of package from CDN

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Normalize } from 'styled-normalize';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,6 +12,7 @@ import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Normalize />
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
