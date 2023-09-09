675acdca233c8f5f2aa2bd1d9bfa6cddad3ccf77
Adam Musial <adammusial@wp.pl>
2023-09-09 15:56:40 +0200

    launching HashRouter with navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { HashRouter, Route, Switch, Link, Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author/Author";
 
-const App = () => <Tasks />;
+function App() {
+    return (
+        <HashRouter>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/zadania">
+                            Zadania
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/autor">
+                            O autorze
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path="/zadania">
+                    <Tasks />
+                </Route>
+                <Route path="/autor">
+                    <Author />
+                </Route>
+                <Route path="/">
+                    <Redirect to="/zadania" />
+                </Route>
+            </Switch>
+        </HashRouter>
+    )
+};
 
 export default App;
